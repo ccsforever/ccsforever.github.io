0a3a1ed44444968ea8815fb5c7c648ea46881744
Hyowook Park <ccsforever@gmail.com>
2024-12-18 22:54:12 +0900

    [feat]edit product page

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import { useSearchParams, useParams } from 'react-router-dom';
+import React from "react";
+import { useSearchParams, useParams } from "react-router-dom";
 
-import UnityContainer from '../components/UnityContainer';
+import UnityContainer from "../components/UnityContainer";
 
 const Product = () => {
-    const path = "/product";
-    const { id } = useParams();
+  const path = "/product";
+  const { id } = useParams();
 
-    const [serchParams] = useSearchParams();
-    const query = serchParams.get("search") as string;
+  const [serchParams] = useSearchParams();
+  const query = serchParams.get("search") as string;
 
-    return (
-        <div>
-            <p>path {path}</p>
-            <p>productId {id}</p>
-            <p>query {query}</p>
-            <UnityContainer path={`${path}/${id as string}/`} query={query} />
-        </div >
-    );
-}
+  return (
+    <div className="asdf">
+      <UnityContainer path={`${path}/${id as string}/`} query={query} />
+    </div>
+  );
+};
 
 export default Product;
